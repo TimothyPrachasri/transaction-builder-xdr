8fd59e8f394d5fcfcf64043bb935dfa9140db5f6
Tim <tim@Tims-MacBook-Pro.local>
2018-11-06 13:15:20 +0700

    feat: add signing method

--- transaction/builder.go
+++ transaction/builder.go
@@ -1,30 +1,42 @@
 package transactionbuilder
 
 import (
+	"github.com/pkg/errors"
+	"github.com/stellar/go/keypair"
+	"github.com/stellar/go/network"
 	"github.com/stellar/go/xdr"
 )
 
 // TransactionBuilder is a struct contain xdrTransaction
 type TransactionBuilder struct {
 	TransactionXDR *xdr.Transaction
+	Signatures     []xdr.DecoratedSignature `xdrmaxsize:"20"`
 }
 
 // OperationForm is a form for building operationForm and send it to makeAll method.
 type OperationForm struct {
-	operationType        xdr.OperationType
-	paymentOperationForm interface{}
+	OperationType        xdr.OperationType
+	PaymentOperationForm interface{}
 }
 
-func (transactionBuilder *TransactionBuilder) make(operationType xdr.OperationType, paymentOperationForm interface{}) (err error) {
+// SignerForm is a form for transaction signing protocol.
+type SignerForm struct {
+	Signer            string
+	NetworkPassPhrase string
+}
+
+// Make method is a method in which TransactionBuilder make a TransactionXDR based on operationType and paymentOperationForm.
+func (transactionBuilder *TransactionBuilder) Make(operationType xdr.OperationType, paymentOperationForm interface{}) (err error) {
 	operation := xdr.Operation{SourceAccount: &xdr.AccountId{}}
 	operation.Body, err = xdr.NewOperationBody(operationType, paymentOperationForm)
 	transactionBuilder.TransactionXDR.Operations = append(transactionBuilder.TransactionXDR.Operations, operation)
 	return
 }
 
-func (transactionBuilder *TransactionBuilder) makeAll(forms []OperationForm) (err error) {
+// MakeAll method is like a Make method but makeAll receives an array of OperationForm which contain operationType and paymentOperationForm.
+func (transactionBuilder *TransactionBuilder) MakeAll(forms []OperationForm) (err error) {
 	for _, ech := range forms {
-		err = transactionBuilder.make(ech.operationType, ech.paymentOperationForm)
+		err = transactionBuilder.Make(ech.OperationType, ech.PaymentOperationForm)
 		if err != nil {
 			break
 		}
@@ -32,7 +44,41 @@ func (transactionBuilder *TransactionBuilder) makeAll(forms []OperationForm) (er
 	return
 }
 
-func getInstance() TransactionBuilder {
-	result := TransactionBuilder{&xdr.Transaction{}}
+// Sign method will be called after make or makeAll method for transaction signing protocol.
+// It will receive signer and networkPassPhrase as a string.
+func (transactionBuilder *TransactionBuilder) Sign(signer, networkPassPhrase string) (err error) {
+	hash, err := network.HashTransaction(transactionBuilder.TransactionXDR, networkPassPhrase)
+	if err != nil {
+		return errors.Wrap(err, "hash tx failed")
+	}
+
+	kp, err := keypair.Parse(signer)
+	if err != nil {
+		return errors.Wrap(err, "parse failed")
+	}
+
+	sig, err := kp.SignDecorated(hash[:])
+	if err != nil {
+		return errors.Wrap(err, "sign tx failed")
+	}
+
+	transactionBuilder.Signatures = append(transactionBuilder.Signatures, sig)
+	return nil
+}
+
+// SignAll is similar to Sign method but it will receive an array of SignerForm which contains signer and networkPassPhrase.
+func (transactionBuilder *TransactionBuilder) SignAll(forms []SignerForm) (err error) {
+	for _, ech := range forms {
+		err := transactionBuilder.Sign(ech.Signer, ech.NetworkPassPhrase)
+		if err != nil {
+			break
+		}
+	}
+	return
+}
+
+// GetInstance is like a new object method in OOP
+func GetInstance() TransactionBuilder {
+	result := TransactionBuilder{TransactionXDR: &xdr.Transaction{}}
 	return result
 }
