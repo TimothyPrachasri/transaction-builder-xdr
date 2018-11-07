999ed0901140549c16201b9a3ba2600e98cce5bc
Tim <tim@Tims-MacBook-Pro.local>
2018-11-07 12:46:12 +0700

    test: two more test case in payment builder test

--- transaction/builder/payment_builder_test.go
+++ transaction/builder/payment_builder_test.go
@@ -1,7 +1,8 @@
 package transactionbuilder_test
 
 import (
-	"fmt"
+	"encoding/base64"
+	"strings"
 	builder "transaction-builder-xdr/transaction/builder"
 	enveloper "transaction-builder-xdr/transaction/envelope"
 
@@ -61,7 +62,7 @@ var _ = Describe("Creating Transaction XDR", func() {
 	})
 
 	It("should return a correct xdr enveloped transaction", func() {
-		By("Adding Payment Operation")
+		By("Adding One Payment Operation")
 		var (
 			tB64   string
 			txeB64 string
@@ -72,19 +73,43 @@ var _ = Describe("Creating Transaction XDR", func() {
 			SeqNum:        xdr.SequenceNumber(1),
 			Memo:          Memo,
 		}
-		fmt.Println(opB64, "base 64 bitch")
 		transactionBuilder := builder.GetInstance(&tx)
 		transactionBuilder.MakeOperation(opB64)
-		fmt.Println(transactionBuilder.TransactionXDR)
 		tB64, err = transactionBuilder.ToBase64()
-		fmt.Println("pass0")
 		Expect(err).NotTo(HaveOccurred())
-		fmt.Println("pass1")
 		transactionEnvelope := enveloper.GetInstance(tB64)
 		err = transactionEnvelope.Sign("SDKJ2BUKQ5TCMSLRQBAFSEVJ3LBXFGHEKKPTYNCDWSOJ4CFGFR5SKRME", "Test SDF Network ; September 2015")
-		fmt.Println("pass2")
 		Expect(err).NotTo(HaveOccurred())
 		txeB64, err = transactionEnvelope.ToBase64()
 		Expect(txeB64).Should(Equal("AAAAABjCG5iSDJdtHOz38Hfkb0RYQP11Tu5cdDF+Teqp/7GLAAAACgAAAAAAAAABAAAAAAAAAAAAAAABAAAAAAAAAAEAAAAAkCqQJepwMIpx3O5TN76xsBApVqnpDD2f0X2SQ+EcMz4AAAAAAAAAAB3NZQAAAAAAAAAAAan/sYsAAABA3LBOukhEwmXdSSi7zTUC5MfZM8opRWI0SZ8cscGO/av0aJXfZzRtGXiWE2cK7voF1GswJUiEpNo/EEWHYvzjCA=="))
+	})
+
+	It("should return a correct unmarshalled bytes and operation", func() {
+		By("Adding One Payment Operation")
+		var (
+			tB64      string
+			txeB64    string
+			txe       xdr.TransactionEnvelope
+			bytesRead int
+		)
+		tx := xdr.Transaction{
+			SourceAccount: Source,
+			Fee:           10,
+			SeqNum:        xdr.SequenceNumber(1),
+			Memo:          Memo,
+		}
+		transactionBuilder := builder.GetInstance(&tx)
+		transactionBuilder.MakeOperation(opB64)
+		tB64, err = transactionBuilder.ToBase64()
+		Expect(err).NotTo(HaveOccurred())
+		transactionEnvelope := enveloper.GetInstance(tB64)
+		err = transactionEnvelope.Sign("SDKJ2BUKQ5TCMSLRQBAFSEVJ3LBXFGHEKKPTYNCDWSOJ4CFGFR5SKRME", "Test SDF Network ; September 2015")
+		Expect(err).NotTo(HaveOccurred())
+		txeB64, err = transactionEnvelope.ToBase64()
+		rawr := strings.NewReader(txeB64)
+		b64r := base64.NewDecoder(base64.StdEncoding, rawr)
+		bytesRead, err = xdr.Unmarshal(b64r, &txe)
+		Expect(bytesRead).Should(Equal(196))
+		Expect(len(txe.Tx.Operations)).Should(Equal(1))
 	})
 })
