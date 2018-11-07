b781210a302f7d3e41fa65dad6e736ab1243efd5
Tim <tim@Tims-MacBook-Pro.local>
2018-11-07 16:48:55 +0700

    refactor: move beforeSuite function to builder_suite_test.go which is a main for testing

--- transaction/builder/payment_builder_test.go
+++ transaction/builder/payment_builder_test.go
@@ -7,33 +7,8 @@ import (
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	"github.com/stellar/go/keypair"
 	"github.com/stellar/go/xdr"
 )
-
-var (
-	SourceSeed string
-	DestAddr   string
-	PassPhrase string
-	Skp        keypair.KP
-	Dkp        keypair.KP
-	Source     xdr.AccountId
-	Memo       xdr.Memo
-	err        error
-)
-
-var _ = BeforeSuite(func() {
-	SourceSeed = "SDKJ2BUKQ5TCMSLRQBAFSEVJ3LBXFGHEKKPTYNCDWSOJ4CFGFR5SKRME"
-	DestAddr = "GCICVEBF5JYDBCTR3TXFGN56WGYBAKKWVHUQYPM72F6ZEQ7BDQZT4NFZ"
-	Skp = keypair.MustParse(SourceSeed)
-	Dkp = keypair.MustParse(DestAddr)
-	PassPhrase = "Test SDF Network ; September 2015"
-	Memo, err = xdr.NewMemo(xdr.MemoTypeMemoNone, nil)
-	err = Source.SetAddress(Skp.Address())
-	if err != nil {
-		panic(err)
-	}
-})
 
 var _ = Describe("Creating transaction XDR with payment operation", func() {
 	var (
