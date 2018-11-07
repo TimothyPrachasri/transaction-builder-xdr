41573bf66a114683768a1a458401a73eb8835e6e
Tim <tim@Tims-MacBook-Pro.local>
2018-11-07 23:28:40 +0700

    test: add test cases for creating transaction with added fields

---
+++ transaction/builder/builder_test.go
@@ -0,0 +1,36 @@
+package transactionbuilder_test
+
+import (
+	builder "transaction-builder-xdr/transaction/builder"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/stellar/go/xdr"
+)
+
+var _ = Describe("Creating Transaction", func() {
+
+	It("should return a correct xdr transaction fields", func() {
+		var (
+			transactionBuilder builder.TransactionBuilder
+			accountId          xdr.AccountId
+		)
+		By("adding source account")
+		transactionBuilder = builder.GetInstance()
+		transactionBuilder.MakeSourceAccount(SourceSeed)
+		accountId = xdr.AccountId{}
+		Expect(transactionBuilder.TransactionXDR.SourceAccount).Should(BeAssignableToTypeOf(accountId))
+
+		By("adding fee")
+		transactionBuilder = builder.GetInstance()
+		transactionBuilder.MakeFee(100)
+		accountId = xdr.AccountId{}
+		Expect(transactionBuilder.TransactionXDR.Fee).Should(BeEquivalentTo(100))
+
+		By("adding sequence number")
+		transactionBuilder = builder.GetInstance()
+		transactionBuilder.MakeSequenceNumber(uint64(1))
+		accountId = xdr.AccountId{}
+		Expect(transactionBuilder.TransactionXDR.SeqNum).Should(BeEquivalentTo(1))
+	})
+})
