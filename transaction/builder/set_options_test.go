66333bff92fbf9251662676042e16a68cf79a51d
Tim <tim@Tims-MacBook-Pro.local>
2018-11-07 18:13:11 +0700

    test: create test cases of set options operation

---
+++ transaction/builder/set_options_test.go
@@ -0,0 +1,73 @@
+package transactionbuilder_test
+
+import (
+	"encoding/base64"
+	"strings"
+	builder "transaction-builder-xdr/transaction/builder"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/stellar/go/xdr"
+)
+
+var _ = Describe("Creating transaction XDR with payment operation", func() {
+	var (
+		opB64 string
+	)
+
+	BeforeEach(func() {
+		inflationDestinationPublicKey := "GCIQJ3JRXEEAKFL22C43X66B4NKACPWZ27WIMNXGA5CIEHOYWNXD3EQR"
+		var destination xdr.AccountId
+		err = destination.SetAddress(inflationDestinationPublicKey)
+		Expect(err).NotTo(HaveOccurred())
+		body := xdr.SetOptionsOp{
+			InflationDest: &destination,
+		}
+		op := xdr.Operation{}
+		op.Body, err = xdr.NewOperationBody(xdr.OperationTypeSetOptions, body)
+		Expect(err).NotTo(HaveOccurred())
+		opB64, err = xdr.MarshalBase64(op)
+		Expect(err).NotTo(HaveOccurred())
+	})
+
+	It("should return a correct xdr transaction string", func() {
+		By("Adding One Payment Operation")
+		var (
+			tB64 string
+		)
+		tx := xdr.Transaction{
+			SourceAccount: Source,
+			Fee:           10,
+			SeqNum:        xdr.SequenceNumber(1),
+			Memo:          Memo,
+		}
+		transactionBuilder := builder.GetInstance(&tx)
+		transactionBuilder.MakeOperation(opB64)
+		tB64, err = transactionBuilder.ToBase64()
+		Expect(err).NotTo(HaveOccurred())
+		Expect(tB64).Should(Equal("AAAAABjCG5iSDJdtHOz38Hfkb0RYQP11Tu5cdDF+Teqp/7GLAAAACgAAAAAAAAABAAAAAAAAAAAAAAABAAAAAAAAAAUAAAABAAAAAJEE7TG5CAUVetC5u/vB41QBPtnX7IY25gdEgh3Ys249AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"))
+	})
+
+	It("should return a correct unmarshalled bytes and operation", func() {
+		By("Adding One Payment Operation")
+		var (
+			tB64           string
+			unmarshalledTx xdr.Transaction
+			bytesRead      int
+		)
+		tx := xdr.Transaction{
+			SourceAccount: Source,
+			Fee:           10,
+			SeqNum:        xdr.SequenceNumber(1),
+			Memo:          Memo,
+		}
+		transactionBuilder := builder.GetInstance(&tx)
+		transactionBuilder.MakeOperation(opB64)
+		tB64, err = transactionBuilder.ToBase64()
+		rawr := strings.NewReader(tB64)
+		b64r := base64.NewDecoder(base64.StdEncoding, rawr)
+		bytesRead, err = xdr.Unmarshal(b64r, &unmarshalledTx)
+		Expect(bytesRead).Should(Equal(144))
+		Expect(len(unmarshalledTx.Operations)).Should(Equal(1))
+	})
+})
