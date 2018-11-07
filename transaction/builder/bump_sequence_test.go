4b7a960f85ef7160818f70ea04ede5d4ff5cd273
Tim <tim@Tims-MacBook-Pro.local>
2018-11-07 18:07:07 +0700

    test: create test cases of bump sequence operation

---
+++ transaction/builder/bump_sequence_test.go
@@ -0,0 +1,71 @@
+package transactionbuilder_test
+
+import (
+	"encoding/base64"
+	"strings"
+	builder "transaction-builder-xdr/transaction/builder"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/stellar/go/xdr"
+)
+
+var _ = Describe("Creating transaction XDR with payment operation", func() {
+	var (
+		opB64 string
+	)
+
+	BeforeEach(func() {
+		var sequenceNumber uint64
+		sequenceNumber = 1008097543847999
+		body := xdr.BumpSequenceOp{
+			BumpTo: xdr.SequenceNumber(sequenceNumber),
+		}
+		op := xdr.Operation{}
+		op.Body, err = xdr.NewOperationBody(xdr.OperationTypeBumpSequence, body)
+		Expect(err).NotTo(HaveOccurred())
+		opB64, err = xdr.MarshalBase64(op)
+		Expect(err).NotTo(HaveOccurred())
+	})
+
+	It("should return a correct xdr transaction string", func() {
+		By("Adding One Payment Operation")
+		var (
+			tB64 string
+		)
+		tx := xdr.Transaction{
+			SourceAccount: Source,
+			Fee:           10,
+			SeqNum:        xdr.SequenceNumber(1),
+			Memo:          Memo,
+		}
+		transactionBuilder := builder.GetInstance(&tx)
+		transactionBuilder.MakeOperation(opB64)
+		tB64, err = transactionBuilder.ToBase64()
+		Expect(err).NotTo(HaveOccurred())
+		Expect(tB64).Should(Equal("AAAAABjCG5iSDJdtHOz38Hfkb0RYQP11Tu5cdDF+Teqp/7GLAAAACgAAAAAAAAABAAAAAAAAAAAAAAABAAAAAAAAAAsAA5TcAAAAPwAAAAA="))
+	})
+
+	It("should return a correct unmarshalled bytes and operation", func() {
+		By("Adding One Payment Operation")
+		var (
+			tB64           string
+			unmarshalledTx xdr.Transaction
+			bytesRead      int
+		)
+		tx := xdr.Transaction{
+			SourceAccount: Source,
+			Fee:           10,
+			SeqNum:        xdr.SequenceNumber(1),
+			Memo:          Memo,
+		}
+		transactionBuilder := builder.GetInstance(&tx)
+		transactionBuilder.MakeOperation(opB64)
+		tB64, err = transactionBuilder.ToBase64()
+		rawr := strings.NewReader(tB64)
+		b64r := base64.NewDecoder(base64.StdEncoding, rawr)
+		bytesRead, err = xdr.Unmarshal(b64r, &unmarshalledTx)
+		Expect(bytesRead).Should(Equal(80))
+		Expect(len(unmarshalledTx.Operations)).Should(Equal(1))
+	})
+})
