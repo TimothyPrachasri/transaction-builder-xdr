9a2254d60b5f887a9620472e2926604afc605ed0
Tim <tim@Tims-MacBook-Pro.local>
2018-11-07 15:46:51 +0700

    test: add test cases of create account operation and add more expect error in payment builder test

--- transaction/builder/create_account_test.go
+++ transaction/builder/create_account_test.go
@@ -7,33 +7,8 @@ import (
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	"github.com/stellar/go/keypair"
 	"github.com/stellar/go/xdr"
 )
-
-var (
-	SourceSeed string
-	DestAddr   string
-	PassPhrase string
-	Skp        keypair.KP
-	Dkp        keypair.KP
-	Source     xdr.AccountId
-	Memo       xdr.Memo
-	err        error
-)
-
-var _ = BeforeSuite(func() {
-	SourceSeed = "SDKJ2BUKQ5TCMSLRQBAFSEVJ3LBXFGHEKKPTYNCDWSOJ4CFGFR5SKRME"
-	DestAddr = "GCICVEBF5JYDBCTR3TXFGN56WGYBAKKWVHUQYPM72F6ZEQ7BDQZT4NFZ"
-	Skp = keypair.MustParse(SourceSeed)
-	Dkp = keypair.MustParse(DestAddr)
-	PassPhrase = "Test SDF Network ; September 2015"
-	Memo, err = xdr.NewMemo(xdr.MemoTypeMemoNone, nil)
-	err = Source.SetAddress(Skp.Address())
-	if err != nil {
-		panic(err)
-	}
-})
 
 var _ = Describe("Creating transaction XDR with payment operation", func() {
 	var (
@@ -41,18 +16,18 @@ var _ = Describe("Creating transaction XDR with payment operation", func() {
 	)
 
 	BeforeEach(func() {
-		asset, err := xdr.NewAsset(xdr.AssetTypeAssetTypeNative, nil)
-		Expect(err).NotTo(HaveOccurred())
+		destinationPublicKey := "GDKV36XRERL7HVQ5GKRAV47ZLEPIZMFM7MMLEO4NKQOWFPL5NCIEW3GR"
+		var startingBalance uint64
+		startingBalance = 100
 		var destination xdr.AccountId
-		err = destination.SetAddress(Dkp.Address())
+		err = destination.SetAddress(destinationPublicKey)
 		Expect(err).NotTo(HaveOccurred())
-		body := xdr.PaymentOp{
-			Destination: destination,
-			Asset:       asset,
-			Amount:      50 * 10000000,
+		body := xdr.CreateAccountOp{
+			Destination:     destination,
+			StartingBalance: xdr.Int64(startingBalance) * 10000000,
 		}
 		op := xdr.Operation{}
-		op.Body, err = xdr.NewOperationBody(xdr.OperationTypePayment, body)
+		op.Body, err = xdr.NewOperationBody(xdr.OperationTypeCreateAccount, body)
 		Expect(err).NotTo(HaveOccurred())
 		opB64, err = xdr.MarshalBase64(op)
 		Expect(err).NotTo(HaveOccurred())
@@ -73,7 +48,7 @@ var _ = Describe("Creating transaction XDR with payment operation", func() {
 		transactionBuilder.MakeOperation(opB64)
 		tB64, err = transactionBuilder.ToBase64()
 		Expect(err).NotTo(HaveOccurred())
-		Expect(tB64).Should(Equal("AAAAABjCG5iSDJdtHOz38Hfkb0RYQP11Tu5cdDF+Teqp/7GLAAAACgAAAAAAAAABAAAAAAAAAAAAAAABAAAAAAAAAAEAAAAAkCqQJepwMIpx3O5TN76xsBApVqnpDD2f0X2SQ+EcMz4AAAAAAAAAAB3NZQAAAAAA"))
+		Expect(tB64).Should(Equal("AAAAABjCG5iSDJdtHOz38Hfkb0RYQP11Tu5cdDF+Teqp/7GLAAAACgAAAAAAAAABAAAAAAAAAAAAAAABAAAAAAAAAAAAAAAA1V368SRX89YdMqIK8/lZHoywrPsYsjuNVB1ivX1okEsAAAAAO5rKAAAAAAA="))
 	})
 
 	It("should return a correct unmarshalled bytes and operation", func() {
@@ -95,7 +70,7 @@ var _ = Describe("Creating transaction XDR with payment operation", func() {
 		rawr := strings.NewReader(tB64)
 		b64r := base64.NewDecoder(base64.StdEncoding, rawr)
 		bytesRead, err = xdr.Unmarshal(b64r, &unmarshalledTx)
-		Expect(bytesRead).Should(Equal(120))
+		Expect(bytesRead).Should(Equal(116))
 		Expect(len(unmarshalledTx.Operations)).Should(Equal(1))
 	})
 })
