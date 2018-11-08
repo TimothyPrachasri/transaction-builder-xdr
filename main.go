5f646453fd67c4be73d74711cc08fbcdba9b5f4a
Tim <tim@Tims-MacBook-Pro.local>
2018-11-08 18:02:46 +0700

    test: add main for horizon api connecting

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/stellar/go/clients/horizon"
+)
+
+func main() {
+	blob := "AAAAAFsAPNHwcy2ZPYftEEoI+dAPr0ZBN+vuXUKPEKcq2mmtAAAAZAAI8fMAAAABAAAAAAAAAAAAAAABAAAAAAAAAAEAAAAAdfeOkY7szblb3J8lDy2i0o1ssnDcDOkFjxjwFx/sV+gAAAAAAAAAAAAAAGQAAAAAAAAAASraaa0AAABAQVkwp5giAPb+it3dZlGMDzC7YmbaC3ESwUF1ud440EYHSTCsa2lW547GSTZMFfRgb2gOAmuKooD7lH4qYC0dAg=="
+	if _, err := horizon.DefaultTestNetClient.LoadAccount("GD3F7GSWWTP6MQVFO6ZT64TDS7XR2KUPKCFCQVBG4TF6DCYLF5SWJT7Z"); err != nil {
+		fmt.Println("ola1")
+		panic(err)
+	}
+	resp, err := horizon.DefaultTestNetClient.SubmitTransaction(blob)
+	if err != nil {
+		fmt.Println("ola2")
+		panic(err)
+	}
+
+	fmt.Println("transaction posted in ledger:", resp.Ledger)
+}
--- transaction/main_test.go
+++ transaction/main_test.go
@@ -1,8 +1,8 @@
 package test
 
 import (
-	"bytes"
 	"encoding/base64"
+	"encoding/json"
 	"fmt"
 	"log"
 	"strings"
@@ -10,94 +10,13 @@ import (
 	builder "transaction-builder-xdr/transaction/builder"
 	envelope "transaction-builder-xdr/transaction/envelope"
 
-	"github.com/stellar/go/hash"
 	"github.com/stellar/go/keypair"
 	"github.com/stellar/go/xdr"
 )
 
-// ExampleBuildTransaction creates and signs a simple transaction using the
-// build package. The build package is designed to make it easier and more
-// intuitive to configure and sign a transaction.
-func ExampleLowLevelTransaction() {
-	skp := keypair.MustParse("SDKJ2BUKQ5TCMSLRQBAFSEVJ3LBXFGHEKKPTYNCDWSOJ4CFGFR5SKRME")
-	dkp := keypair.MustParse("GCICVEBF5JYDBCTR3TXFGN56WGYBAKKWVHUQYPM72F6ZEQ7BDQZT4NFZ")
-
-	asset, err := xdr.NewAsset(xdr.AssetTypeAssetTypeNative, nil)
-	if err != nil {
-		panic(err)
-	}
-
-	var destination xdr.AccountId
-	err = destination.SetAddress(dkp.Address())
-	if err != nil {
-		panic(err)
-	}
-
-	op := xdr.PaymentOp{
-		Destination: destination,
-		Asset:       asset,
-		Amount:      50 * 10000000,
-	}
-
-	memo, err := xdr.NewMemo(xdr.MemoTypeMemoNone, nil)
-
-	var source xdr.AccountId
-	err = source.SetAddress(skp.Address())
-	if err != nil {
-		panic(err)
-	}
-
-	body, err := xdr.NewOperationBody(xdr.OperationTypePayment, op)
-	if err != nil {
-		panic(err)
-	}
-
-	tx := xdr.Transaction{
-		SourceAccount: source,
-		Fee:           10,
-		SeqNum:        xdr.SequenceNumber(1),
-		Memo:          memo,
-		Operations: []xdr.Operation{
-			{Body: body},
-		},
-	}
-
-	var txBytes bytes.Buffer
-	_, err = xdr.Marshal(&txBytes, tx)
-	if err != nil {
-		panic(err)
-	}
-
-	txHash := hash.Hash(txBytes.Bytes())
-	signature, err := skp.Sign(txHash[:])
-	if err != nil {
-		panic(err)
-	}
-
-	ds := xdr.DecoratedSignature{
-		Hint:      skp.Hint(),
-		Signature: xdr.Signature(signature[:]),
-	}
-
-	txe := xdr.TransactionEnvelope{
-		Tx:         tx,
-		Signatures: []xdr.DecoratedSignature{ds},
-	}
-
-	var txeBytes bytes.Buffer
-	_, err = xdr.Marshal(&txeBytes, txe)
-	if err != nil {
-		panic(err)
-	}
-	txeB64 := base64.StdEncoding.EncodeToString(txeBytes.Bytes())
-
-	fmt.Printf("tx base64: %s", txeB64)
-	// Output: tx base64: AAAAABjCG5iSDJdtHOz38Hfkb0RYQP11Tu5cdDF+Teqp/7GLAAAACgAAAAAAAAABAAAAAAAAAAAAAAABAAAAAAAAAAEAAAAAkCqQJepwMIpx3O5TN76xsBApVqnpDD2f0X2SQ+EcMz4AAAAAAAAAAB3NZQAAAAAAAAAAAan/sYsAAABAIAWtiYQfI5rp4ZGE98rmyvfXvVX0+340nkEjYYDhFVFE7FCJERjlChY+pgR8THv7jgbtEgPZAgwJwXSrZh7mAw==
-}
-
 func ExampleUsingTransactionBuilder() {
-	skp := keypair.MustParse("SDKJ2BUKQ5TCMSLRQBAFSEVJ3LBXFGHEKKPTYNCDWSOJ4CFGFR5SKRME")
-	dkp := keypair.MustParse("GCICVEBF5JYDBCTR3TXFGN56WGYBAKKWVHUQYPM72F6ZEQ7BDQZT4NFZ")
+	skp := keypair.MustParse("SBMRYER7UW2KHXAUKFZC6YVRDX6TLBUL55RE37N7CNAGVRSVRW5GA6I4")
+	dkp := keypair.MustParse("GB27PDURR3WM3OK33SPSKDZNULJI23FSODOAZ2IFR4MPAFY75RL6Q6QR")
 
 	asset, err := xdr.NewAsset(xdr.AssetTypeAssetTypeNative, nil)
 	if err != nil {
@@ -113,7 +32,7 @@ func ExampleUsingTransactionBuilder() {
 	body := xdr.PaymentOp{
 		Destination: destination,
 		Asset:       asset,
-		Amount:      50 * 10000000,
+		Amount:      100,
 	}
 	op := xdr.Operation{}
 	op.Body, err = xdr.NewOperationBody(xdr.OperationTypePayment, body)
@@ -128,11 +47,11 @@ func ExampleUsingTransactionBuilder() {
 	if err != nil {
 		panic(err)
 	}
-
+	var defaultBaseFee uint64 = 100
 	tx := xdr.Transaction{
 		SourceAccount: source,
-		Fee:           10,
-		SeqNum:        xdr.SequenceNumber(1),
+		Fee:           xdr.Uint32(int(defaultBaseFee) * 1),
+		SeqNum:        xdr.SequenceNumber(2517825793032193),
 		Memo:          memo,
 	}
 
@@ -142,25 +61,28 @@ func ExampleUsingTransactionBuilder() {
 		panic(err)
 	}
 	transactionBuilder.MakeOperation(opB64)
+
 	tB64, err := transactionBuilder.ToBase64()
 	if err != nil {
 		panic(err)
 	}
 	transactionEnvelope := envelope.GetInstance(tB64)
-	err = transactionEnvelope.Sign("SDKJ2BUKQ5TCMSLRQBAFSEVJ3LBXFGHEKKPTYNCDWSOJ4CFGFR5SKRME", "Test SDF Network ; September 2015")
+	err = transactionEnvelope.Sign("SBMRYER7UW2KHXAUKFZC6YVRDX6TLBUL55RE37N7CNAGVRSVRW5GA6I4", "Test SDF Network ; September 2015")
 	if err != nil {
 		panic(err)
 	}
+	res, _ := json.Marshal(transactionEnvelope)
+	fmt.Println(string(res))
 	txeB64, err := transactionEnvelope.ToBase64()
 	if err != nil {
 		panic(err)
 	}
 	fmt.Printf("tx base64: %s", txeB64)
-	// Output: tx base64: AAAAABjCG5iSDJdtHOz38Hfkb0RYQP11Tu5cdDF+Teqp/7GLAAAACgAAAAAAAAABAAAAAAAAAAAAAAABAAAAAAAAAAEAAAAAkCqQJepwMIpx3O5TN76xsBApVqnpDD2f0X2SQ+EcMz4AAAAAAAAAAB3NZQAAAAAAAAAAAan/sYsAAABA3LBOukhEwmXdSSi7zTUC5MfZM8opRWI0SZ8cscGO/av0aJXfZzRtGXiWE2cK7voF1GswJUiEpNo/EEWHYvzjCA==
+	// Output: tx base64: AAAAAFsAPNHwcy2ZPYftEEoI+dAPr0ZBN+vuXUKPEKcq2mmtAAAAZAAI8fMAAAABAAAAAAAAAAAAAAABAAAAAAAAAAEAAAAAdfeOkY7szblb3J8lDy2i0o1ssnDcDOkFjxjwFx/sV+gAAAAAAAAAAAAAAGQAAAAAAAAAASraaa0AAABAQVkwp5giAPb+it3dZlGMDzC7YmbaC3ESwUF1ud440EYHSTCsa2lW547GSTZMFfRgb2gOAmuKooD7lH4qYC0dAg==
 }
 
 func ExampleDecodeTransaction() {
-	data := "AAAAABjCG5iSDJdtHOz38Hfkb0RYQP11Tu5cdDF+Teqp/7GLAAAACgAAAAAAAAABAAAAAAAAAAAAAAABAAAAAAAAAAEAAAAAkCqQJepwMIpx3O5TN76xsBApVqnpDD2f0X2SQ+EcMz4AAAAAAAAAAB3NZQAAAAAAAAAAAan/sYsAAABA3LBOukhEwmXdSSi7zTUC5MfZM8opRWI0SZ8cscGO/av0aJXfZzRtGXiWE2cK7voF1GswJUiEpNo/EEWHYvzjCA=="
+	data := "AAAAAFsAPNHwcy2ZPYftEEoI+dAPr0ZBN+vuXUKPEKcq2mmtAAAAZAAI8fMAAAABAAAAAAAAAAAAAAABAAAAAAAAAAEAAAAAdfeOkY7szblb3J8lDy2i0o1ssnDcDOkFjxjwFx/sV+gAAAAAAAAAAAAAAGQAAAAAAAAAASraaa0AAABAQVkwp5giAPb+it3dZlGMDzC7YmbaC3ESwUF1ud440EYHSTCsa2lW547GSTZMFfRgb2gOAmuKooD7lH4qYC0dAg=="
 	rawr := strings.NewReader(data)
 	b64r := base64.NewDecoder(base64.StdEncoding, rawr)
 
